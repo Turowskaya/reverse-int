a4d865ab15076f6c7702e2431ea17aef5374310c
turowskaja@gmail.com <turowskaja@gmail.com>
2020-10-19 22:09:20 +0300

    init: start task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(n).split('').reverse();
+
+    if(arr[arr.length-1] === '-'){
+        arr.pop();
+    }
+
+    return +arr.join('');
+
 }
